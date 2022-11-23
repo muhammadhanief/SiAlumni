070de8ece55077f7037f5183a073bf18c485de39
Zaki Ramadhan <zakiramadhanii88@gmail.com>
2022-11-23 10:39:10 +0700

    atur env.example

--- resources/views/formulir_ijazah.blade.php
+++ resources/views/formulir_ijazah.blade.php
@@ -90,7 +90,7 @@
         <div class="col-lg-6">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Lampiran Dokumen Tambahan</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Detail Lainnya</h6>
                 </div>
                 <div class="card-body">
                     <!-- Wajib -->
