39c8b8a6222d36de4815f56795b7b1dbce8dfba6
Zaki Ramadhan <zakiramadhanii88@gmail.com>
2022-11-23 11:49:53 +0700

    minor

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="https://i.postimg.cc/hjT53TQ5/stis.png" class="logo" alt="Laravel Logo"> <br>
+            <img src="https://i.postimg.cc/hjT53TQ5/stis.png" class="logo" alt="SiAlumni"> <br>
             <span style="font-size: 20px; font-weight: bold;">Sistem Informasi Alumni</span>
         </a>
     </td>
