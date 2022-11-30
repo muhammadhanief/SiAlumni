f2b899edd0a21c5738e15fb9d28d3b1a86062820
Daffa Faris <daffafaris87@gmail.com>
2022-12-01 05:32:35 +0700

    datatables fixed column

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,34 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+$(document).ready(function () {
+    $('#dataTable').DataTable();
+    $('#table-alumni').DataTable({
+        // fixed columns
+        scrollX: true,
+        scrollY: true,
+        scrollCollapse: true,
+        paging: true,
+        // fixed header
+        fixedHeader: true,
+        // responsive
+        responsive: true,
+        // search
+        searching: true,
+        // ordering
+
+        // info
+        info: true,
+        // length change
+        lengthChange: true,
+        // length menu
+        // lengthMenu: [
+
+
+        //     [10, 25, 50, -1],
+        //     [10, 25, 50, "All"]
+        // ],
+        fixedColumns: {
+            left: 1,
+            right: 1
+        }
+    });
 });
